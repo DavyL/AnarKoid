37079d48958be12c5eefeb9a4f71f4e4b784cf5d
DavyL <cr4zypxl@gmail.com>
2019-10-09 16:08:23 +0200

    Added CMake stuff

---
+++ CMakeFiles/anarkoid.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/anarkoid.dir/src/main.c.o"
+  "anarkoid.pdb"
+  "anarkoid"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/anarkoid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
